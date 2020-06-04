12f43fb6e242e85ea0ef099867a034e13e18f7d9
Steven Zaldivar <zh15002@ues.edu.sv>
2020-06-04 04:06:06 -0600

    Update model fields and views
    Views are now implemented,  also a base template is fully functional

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    email = models.EmailField(null=True)
-    active = models.BooleanField(default=True)
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -17,11 +20,31 @@ class Post(models.Model):
     content = models.TextField()
     likes = models.IntegerField(default=0)
     active = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+    slug = models.SlugField()
+
+    def comments(self):
+        return self.comment_set.all()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
+    
 
 class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
     likes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return f'Comment #{self.id}'
 
-    
+def pre_save_post(sender, instance, *args, **kwargs):
+    slug = slugify(instance.title)
+    instance.slug = slug
+
+pre_save.connect(pre_save_post, sender=Post)
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, PostView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list')
+    path('', PostListView.as_view(), name='post_list'),
+    path('<slug>/', PostView.as_view(), name='post_detail')
 ]
